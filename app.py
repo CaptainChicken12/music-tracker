1fcc0a4a227ff5e6956bcf4a34cdb7d43b13787d
CaptainChicken12 <natebenishek@gmail.com>
2025-09-04 16:05:43 -0500

    created html files and routes for the pages: saved, listen_later, and search

--- app.py
+++ app.py
@@ -10,3 +10,16 @@ Session(app)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/saved")
+def saved():
+    return render_template("saved.html")
+
+@app.route("/search")
+def search():
+    return render_template("search.hmtl")
+
+@app.route("listen_later")
+def listen_later():
+    return render_template("listen_later.html")
+
