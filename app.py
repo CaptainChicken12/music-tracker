49f98bf31941563ef73460e50105b6da98c2e4e2
CaptainChicken12 <natebenishek@gmail.com>
2025-09-03 23:46:40 -0500

    Setup index.html route

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
-hi =  12
-yo = 15
+from flask import FLask, render_template, redirect, request, session
+from flask_session import Session
+
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
